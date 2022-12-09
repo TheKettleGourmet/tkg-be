a51b4bd0a712f6632add437f61187ce3098c95f2
leeleonard98 <71783105+leeleonard98@users.noreply.github.com>
2022-12-09 22:53:55 +0800

    fix: shopee bug

--- public/controllers/shopeeController.js
+++ public/controllers/shopeeController.js
@@ -76,23 +76,27 @@ const getAllOrders = async (req) => {
   if (error) {
     log.error('ERR_SHOPEE_GET-ALL-SHOPEE-ORDERS', error.message);
   } else {
-    const orderList = await shopeeApi.getAllOrders({
-      access_token: access_token.value,
-      time_from,
-      time_to,
-      page_size
-    });
-    const orderSN = orderList.response.order_list;
-    if (orderSN.length > 0) {
-      const orders = await orderSN.map((order) => order.order_sn);
-      const response = await shopeeApi.getOrderDetails({
+    try {
+      const orderList = await shopeeApi.getAllOrders({
         access_token: access_token.value,
-        orders
+        time_from,
+        time_to,
+        page_size
       });
-      log.out('OK_SHOPEE_GET-ALL-SHOPEE-ORDERS');
-      return response.response.order_list;
-    } else {
-      return [];
+      const orderSN = orderList.response.order_list;
+      if (orderSN.length > 0) {
+        const orders = await orderSN.map((order) => order.order_sn);
+        const response = await shopeeApi.getOrderDetails({
+          access_token: access_token.value,
+          orders
+        });
+        log.out('OK_SHOPEE_GET-ALL-SHOPEE-ORDERS');
+        return response.response.order_list;
+      } else {
+        return [];
+      }
+    } catch (err) {
+      console.log(err);
     }
   }
 };
